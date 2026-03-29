41a0ea9a9d389abf321e66f71ec0307699b0d103
BuildTools <unconfigured@null.spigotmc.org>
2026-03-30 08:28:20 +1000

    Initial Commit

---
+++ Patches.cs
@@ -0,0 +1,57 @@
+﻿using BepInEx;
+using HarmonyLib;
+using System.IO;
+using UnityEngine;
+
+namespace Deep_Ports
+{
+    internal class Patches
+    {
+        [HarmonyPatch(typeof(FloatingOriginManager))]
+        public static class FloatingOriginManagerPatches
+        {
+            [HarmonyPrefix]
+            [HarmonyPatch("Start")]
+            public static void StartPatch(FloatingOriginManager __instance)
+            {
+                if(__instance.name == "_shifting world")
+                {
+                    TerrainSetup();
+
+                    if(terrain)
+                    {
+                        Debug.LogWarning("Loading custom ports");
+
+                        Transform fa = GameObject.Find("island 15 M (Fort)").transform.Find("Terrain");
+                        fa.GetComponent<Terrain>().terrainData = terrain;
+                        fa.GetComponent<TerrainCollider>().terrainData = terrain;
+
+                        Debug.LogWarning("Custom ports loaded");
+                    }
+                }
+            }
+        }
+
+        private static void TerrainSetup()
+        {
+            string path = Paths.PluginPath + "\\Leopard\\deepterrain";
+            if (!File.Exists(path))
+            {
+                Debug.LogError("Deep Ports not installed correctly");
+                terrainInstalled = false;
+            }
+            else
+            {
+                AssetBundle bundle = AssetBundle.LoadFromFile(path);
+                string td = "fortaestrin";
+                terrain = bundle.LoadAsset<TerrainData>(td);
+                terrainInstalled = true;
+                Debug.LogWarning("Port assets loaded");
+            }
+        }
+
+        public static TerrainData terrain;
+
+        public static bool terrainInstalled;
+    }
+}
---
+++ PortPatcher.cs
@@ -0,0 +1,19 @@
+﻿using BepInEx;
+using HarmonyLib;
+using System.Reflection;
+
+namespace Deep_Ports
+{
+    [BepInPlugin("com.winter.deepports","Deep Ports", "0.1")]
+    public class PortPatcher : BaseUnityPlugin
+    {
+        private void Awake()
+        {
+            Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly(), "com.winter.deepports");
+        }
+
+        public const string pluginGuid = "com.winter.deepports";
+        public const string pluginName = "Deep Ports";
+        public const string pluginVersion = "0.1";
+    }
+}
