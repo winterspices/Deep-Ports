41056dbf4fa7092b0c4bd1c39d15c413eb615ff1
BuildTools <unconfigured@null.spigotmc.org>
2026-04-02 09:10:35 +1000

    Added Dragon Cliffs

--- Patches.cs
+++ Patches.cs
@@ -1,5 +1,6 @@
 ﻿using BepInEx;
 using HarmonyLib;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using UnityEngine;
@@ -19,7 +20,7 @@ namespace Deep_Ports
                 {
                     TerrainSetup();
 
-                    if(data.Count > 0)
+                    if (data.Count > 0)
                     {
                         Transform fa = GameObject.Find("island 15 M (Fort)").transform.Find("Terrain");
                         fa.GetComponent<Terrain>().terrainData = data["fa"];
@@ -29,6 +30,12 @@ namespace Deep_Ports
                         grc.GetComponent<Terrain>().terrainData = data["grc"];
                         grc.GetComponent<TerrainCollider>().terrainData = data["grc"];
 
+                        Transform dc = GameObject.Find("island 9 E (dragon cliffs)").transform.Find("Terrain");
+                        dc.GetComponent<Terrain>().terrainData = data["dc"];
+                        dc.GetComponent<TerrainCollider>().terrainData = data["dc"];
+                        GameObject.Find("island 9 E (dragon cliffs)").transform.Find("Cube_003").transform.localPosition = new Vector3(525f, -14f, -218f);
+                        GameObject.Find("island 9 E (dragon cliffs)").transform.Find("Cube_005").transform.localPosition = new Vector3(525f, 3f, -233f);
+
                         Debug.LogWarning("Custom ports loaded");
                     }
                 }
@@ -37,22 +44,30 @@ namespace Deep_Ports
 
         private static void TerrainSetup()
         {
-            string path = Paths.PluginPath + "\\Deep Ports\\deepports";
-            if (!File.Exists(path))
+            try
             {
-                Debug.LogError("Deep Ports not installed correctly");
-                terrainInstalled = false;
-            }
-            else
+                string path = Paths.PluginPath + "\\Deep Ports\\deepports";
+                if (!File.Exists(path))
+                {
+                    Debug.LogError("Deep Ports not installed correctly");
+                    terrainInstalled = false;
+                }
+                else
+                {
+                    AssetBundle bundle = AssetBundle.LoadFromFile(path);
+
+                    data["fa"] = bundle.LoadAsset<TerrainData>("fortaestrin");
+                    data["grc"] = bundle.LoadAsset<TerrainData>("grc");
+                    data["dc"] = bundle.LoadAsset<TerrainData>("dc");
+
+                    terrainInstalled = true;
+                    Debug.LogWarning("Port assets loaded");
+                }
+            } catch (Exception e)
             {
-                AssetBundle bundle = AssetBundle.LoadFromFile(path);
-
-                data["fa"] = bundle.LoadAsset<TerrainData>("fortaestrin");
-                data["grc"] = bundle.LoadAsset<TerrainData>("grc");
-
-                terrainInstalled = true;
-                Debug.LogWarning("Port assets loaded");
+                Debug.LogError("Uh oh! Something went wrong with loading deep port assets.");
             }
+            
         }
 
         public static Dictionary<string,TerrainData> data = new Dictionary<string, TerrainData>();
